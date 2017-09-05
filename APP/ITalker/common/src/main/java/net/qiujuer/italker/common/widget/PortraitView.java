c166dd003f38e0d9bc8526b00dd5920cfb49246b
Yuyunan <ynn0902@126.com>
2017-09-05 22:07:59 +0800

    提交4.1相关代码

---
+++ APP/ITalker/common/src/main/java/net/qiujuer/italker/common/widget/PortraitView.java
@@ -0,0 +1,24 @@
+package net.qiujuer.italker.common.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by Ynn on 2017/9/5.
+ */
+
+public class PortraitView extends CircleImageView {
+    public PortraitView(Context context) {
+        super(context);
+    }
+
+    public PortraitView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PortraitView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
